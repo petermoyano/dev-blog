bfe41dbddd9848428e4ed6432ef63473d458fe75
Peter M <pmoyano@solvd.com>
2023-02-14 16:43:58 -0300

    install gray matter and add more posts

---
+++ pages/api/posts.ts
@@ -0,0 +1,30 @@
+import { NextApiHandler } from "next";
+import fs, { read } from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const handler: NextApiHandler = (req, res) => {
+  const { method } = req;
+  switch (method) {
+    case "GET": {
+      const data = readPostsInfo();
+      return res.json({ postInfo: data });
+    }
+    default:
+      return res.status(404).send("Not Found");
+  }
+  return res.json({ ok: true });
+};
+
+const readPostsInfo = () => {
+  const dirPathToRead = path.join(process.cwd(), "posts");
+  const dirs = fs.readdirSync(dirPathToRead);
+  const data = dirs.map((filename) => {
+    const filePathToRead = path.join(process.cwd(), "posts/" + filename);
+    const fileContent = fs.readFileSync(filePathToRead, { encoding: "utf-8" });
+    return matter(fileContent).data;
+  });
+  return data;
+};
+
+export default handler;
